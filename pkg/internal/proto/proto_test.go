0d1ff9b9d2975ef923318e6e3d14307c7d9e1348
Jacob Hansson <jakewins@gmail.com>
2016-09-26 15:35:40 -0500

    Little DSL to make protocol tests easier to write

--- pkg/internal/proto/proto_test.go
+++ pkg/internal/proto/proto_test.go
@@ -6,48 +6,80 @@ import (
 	"github.com/jakewins/reactivesocket-go/pkg/internal/frame/keepalive"
 	"github.com/jakewins/reactivesocket-go/pkg/internal/proto"
 	"testing"
+	"github.com/jakewins/reactivesocket-go/pkg/rs"
+	"fmt"
 )
 
-func TestKeepAliveRespond(t *testing.T) {
-	r := recorder{}
-	p := proto.NewProtocol(nil, r.Record)
-
-	p.HandleFrame(keepalive.New(true))
-
-	r.Expect(t, keepalive.New(false))
+type scenario struct {
+	name string
+	handler *rs.RequestHandler
+	exchanges []exchange
+}
+type exchange struct {
+	in []*frame.Frame
+	out []*frame.Frame
 }
 
-func TestKeepAliveDontRespond(t *testing.T) {
-	r := recorder{}
-	p := proto.NewProtocol(nil, r.Record)
+var scenarios = []scenario{
+	scenario{ "Simple keepalive(response plz) -> keepalive", nil, exchanges{
+			exchange{
+				in{keepalive.New(true)},
+				out{keepalive.New(false)},
+			},
+		},
+	},
+	scenario{ "Keepalive with no response", nil, exchanges{
+			exchange{
+				in{keepalive.New(false)},
+				out{},
+			},
+		},
+	},
+}
+type in []*frame.Frame
+type out []*frame.Frame
+type exchanges []exchange
 
-	p.HandleFrame(keepalive.New(false))
+func TestScenarios(t *testing.T) {
+	for _, scenario := range scenarios {
+		r := recorder{}
+		p := proto.NewProtocol(nil, r.Record)
 
-	r.Expect(t) // Eg. no responses
+		for _, exchange := range scenario.exchanges {
+			for _, f := range exchange.in {
+				p.HandleFrame(f)
+			}
+			if err := r.AssertRecorded(exchange.out); err != nil {
+				t.Errorf("Scenario `%s` failed: %s", scenario.name, err.Error())
+			}
+		}
+	}
 }
 
 type recorder struct {
 	recording []*frame.Frame
 }
-
 func (r *recorder) Record(f *frame.Frame) {
 	r.recording = append(r.recording, f.Copy(nil))
 }
-func (r *recorder) Expect(t *testing.T, expected ...*frame.Frame) {
+func (r *recorder) Rewind() {
+	r.recording = nil
+}
+func (r *recorder) AssertRecorded(expected []*frame.Frame) error {
 	if len(expected) < len(r.recording) {
 		for i := len(expected); i < len(r.recording); i++ {
-			t.Errorf("Expected no more than %d frames, frame %d is %s", len(expected), i+1, r.recording[i].Describe())
+			return fmt.Errorf("Expected no more than %d frames, frame %d is %s", len(expected), i+1, r.recording[i].Describe())
 		}
-		return
+		return nil
 	}
 	for idx, expect := range expected {
 		if len(r.recording) <= idx {
-			t.Errorf("Expected frame %d to be %s, but there are no more recorded frames.", idx, expect.Describe())
-			continue
+			return fmt.Errorf("Expected frame %d to be %s, but there are no more recorded frames.", idx, expect.Describe())
 		}
 		found := r.recording[idx]
 		if !bytes.Equal(found.Buf, expect.Buf) {
-			t.Errorf("Expected frame %d to be %s, found %s", idx, expect.Describe(), found.Describe())
+			return fmt.Errorf("Expected frame %d to be %s, found %s", idx, expect.Describe(), found.Describe())
 		}
 	}
+	return nil
 }
