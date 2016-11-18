40e15573e572bd4a59f3e2f2e00ed24a6afdb1df
Jacob Hansson <jakewins@gmail.com>
2016-11-17 22:48:32 -0600

    Add tcp example, start expanding readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ReactiveSocket-go
 
-This is an in-progress implementation of the ReactiveSocket protocol.
-It is not yet usable - stay tuned for updates.
+This is a client and server implementation of the [ReactiveSocket Protocol](http://reactivesocket.io/).
+
+The project is alpha-level currently - all major functionality is in place, but the API is not stable and there are likely many
+edge cases and bugs remaining.
---
+++ pkg/transport/tcp/example_test.go
@@ -0,0 +1,53 @@
+package tcp_test
+
+import (
+	"fmt"
+	"github.com/jakewins/reactivesocket-go/pkg/rs"
+	"github.com/jakewins/reactivesocket-go/pkg/transport/tcp"
+)
+
+func ExampleClient() {
+	socket, err := tcp.Dial("localhost:5678", rs.NewSetupPayload("text/json", "text/json", nil, nil))
+
+	if err != nil {
+		panic(err)
+	}
+
+	stream := socket.RequestSubscription(rs.NewPayload([]byte{1, 2, 3}, []byte{4, 5, 5}))
+	stream.Subscribe(rs.NewSubscriber(func(s rs.Subscription) {
+		s.Request(100)
+	}, func(next rs.Payload) {
+		// Handle payload
+	}, func(err error) {
+		// Handle errors..
+	}, func() {
+		// On complete
+	}))
+}
+
+func ExampleServer() {
+	server, err := tcp.Listen(":0", func(setup rs.ConnectionSetupPayload, socket rs.ReactiveSocket) (*rs.RequestHandler, error) {
+		// Choose a request handler based on the setup payload. Each connection has its own handler.
+		return &rs.RequestHandler{
+			// See rs.RequestHandler for many other request handler types
+			HandleRequestSubscription: func(initial rs.Payload) rs.Publisher {
+				// Return any stream you like, based on the initial payload
+				return rs.NewEmptyPublisher()
+			},
+		}, nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Starting server!")
+	go server.Serve()
+
+	fmt.Println("Shutting down..")
+	server.Shutdown()
+	server.AwaitShutdown()
+	// Output:
+	// Starting server!
+	// Shutting down..
+}
